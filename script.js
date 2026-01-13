92f92c26b001e891c7a1ba64af1d3160235cc01d
Raushan Pandey <raushanpandeyyy@gmail.com>
2026-01-13 12:05:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function for Book Now button
+function bookNow() {
+  alert("Thanks for choosing Dream Travel!");
+}
+
+// Handle contact form submit
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Your message has been sent! We will contact you soon.");
+});
